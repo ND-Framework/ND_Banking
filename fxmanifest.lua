f2d9f2b3b2086aaf02ebed85dbcb2052611c7eab
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-12-08 17:27:40 +0100

    feat: bank home page

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+-- For support join my discord: https://discord.gg/Z9Mxu72zZ6
+
+author "Andyyy#7666"
+description "ND Banking resource for ND Framework"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+files {
+    "ui/index.html",
+    "ui/img/**",
+    "ui/style.css",
+    "ui/script.js"
+}
+ui_page "ui/index.html"
+
+shared_scripts {
+    "@ox_lib/init.lua",
+    "config.lua"
+}
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    "server/main.lua"
+}
+client_scripts {
+    "client/main.lua"
+}
