d5aed6fb4de4977fbd8e6ac2c5d45ab27134110a
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2022-12-25 20:37:26 +0100

    Create query.sql

---
+++ query.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `nd_banking_accounts` (
+    `owner` INT(11) NOT NULL,
+    `account_number` VARCHAR(50) NOT NULL,
+    `history` LONGTEXT NOT NULL DEFAULT '[]',
+    `history` JSON NOT NULL DEFAULT '[]',
+	INDEX `FK__characters` (`owner`) USING BTREE,
+	CONSTRAINT `FK__characters` FOREIGN KEY (`owner`) REFERENCES `characters` (`character_id`) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `nd_banking_invoices` (
+	`invoice_id` INT(11) AUTO_INCREMENT,
+	`sender_name` VARCHAR(50) DEFAULT NULL,
+	`receiver_name` VARCHAR(50) DEFAULT NULL,
+	`sender_account` VARCHAR(50) DEFAULT NULL,
+	`receiver_account` VARCHAR(50) DEFAULT NULL,
+	`amount` INT(11) DEFAULT NULL,
+	`created` VARCHAR(50) DEFAULT NULL,
+	`due_in` VARCHAR(50) DEFAULT NULL,
+	`status` VARCHAR(50) DEFAULT NULL,
+	PRIMARY KEY (`invoice_id`) USING BTREE
+);
