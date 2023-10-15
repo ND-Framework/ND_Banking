faf5600759ec2b12fe3ca45cdb749933a24701b1
Andyyy7666 <86536434+Andyyy7666@users.noreply.github.com>
2023-10-16 00:08:15 +0200

    Update accounts.sql

--- database/accounts.sql
+++ database/accounts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `nd_banking_accounts` (
-    `owner` INT(11) NOT NULL,
-    `account_number` VARCHAR(50) NOT NULL,
-    `history` LONGTEXT NOT NULL DEFAULT '[]',
-    INDEX `FK__characters` (`owner`) USING BTREE,
-    CONSTRAINT `FK__characters` FOREIGN KEY (`owner`) REFERENCES `characters` (`character_id`) ON UPDATE CASCADE ON DELETE CASCADE
+    `owner` INT(11) DEFAULT NULL,
+    `account_number` VARCHAR(50) DEFAULT NULL,
+    `history` LONGTEXT DEFAULT '[]',
+    INDEX `owner` (`owner`) USING BTREE,
+    CONSTRAINT `accountowner` FOREIGN KEY (`owner`) REFERENCES `nd_characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
 );
